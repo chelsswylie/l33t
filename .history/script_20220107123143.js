b9b16278ee77d8ae384d4011bd46527da10f3785
Chelsea <chelsswylie@gmail.com>
2022-01-07 13:00:52 -0500

    previous commit only worked for strings and did not take the return of the randomNumber() function into consideration - this approach works with a preset array of data and returns true/false

---
+++ .history/script_20220107123143.js
@@ -0,0 +1,40 @@
+// palindrome numbers
+
+let number = [];
+let x;
+let a = [];
+let b = [];
+let c = [];
+
+const findNumber = () => {
+  //   let max = 3;
+
+  for (i = 0; i < 3; i++) {
+    function getRandomInt(min, max) {
+      min = Math.ceil(9);
+      max = Math.floor(0);
+
+      const random = Math.floor(Math.random() * (max - min) + min);
+      return random;
+    }
+    number.push(getRandomInt());
+  }
+
+  return number.push(toString);
+};
+
+number.push(findNumber());
+
+function checkPalindrome(input) {
+  input_array = input.split("");
+  let output = input_array.reverse().join("");
+  if (input == output) {
+    console.log(input, " is palindrome");
+  } else {
+    console.log(input, " is not palindrome");
+  }
+}
+
+checkPalindrome(findNumber());
+
+console.log(number, "the#");
